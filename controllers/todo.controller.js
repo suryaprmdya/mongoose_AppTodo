686b527eb62c4c2e7537f249fa02a2495cf2720f
suryaprmdya <suryaoktova.p@gmail.com>
2023-11-14 02:13:58 +0700

    error handling

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -3,87 +3,92 @@ const User = require("../models/user");
 
 module.exports = {
   getAllTodo: async (req, res) => {
-    const todos = await Todo.find()
+    try {
+      const todos = await Todo.find();
+      if (todos.length == 0)
+        throw new Error("Semua data todo gagal didapatkan");
 
-    res.json({
-      message: "Semua data todo berhasil didapat",
-      data: todos
-    })
+      res.json({
+        message: "Semua data todo berhasil didapat",
+        data: todos,
+      });
+    } catch (error) {
+      res.json(error.message);
+    }
   },
 
   createTodoUser: async (req, res) => {
-    let data = req.body
+    let data = req.body;
 
-    await Todo.create(data)
+    await Todo.create(data);
 
     res.json({
-        message: "todo user berhasil dibuat"
-    })
+      message: "todo user berhasil dibuat",
+    });
   },
 
   getAllTodoUser: async (req, res) => {
-    const {username} = req.params
+    const { username } = req.params;
 
     try {
-      const _id = await User.findOne({username: username})
-      if (!_id) throw new Error("User tidak ditemukan")
+      const _id = await User.findOne({ username: username });
+      if (!_id) throw new Error("User tidak ditemukan");
 
-      const todos = await Todo.find({userId: _id})
-      if(todos.length == 0) throw new Error("Todo User Tidak Ada")
+      const todos = await Todo.find({ userId: _id });
+      if (todos.length == 0) throw new Error("Todo User Tidak Ada");
 
       res.json({
         message: "Todo user berhasil didapat",
-        data: todos
-      })
-
+        data: todos,
+      });
     } catch (error) {
-      res.json(error.message)
+      res.json(error.message);
     }
   },
 
   getTodoById: async (req, res) => {
-    const {id} = req.params
+    const { id } = req.params;
 
-    const todo = await Todo.find({_id: id})
-    
+    const todo = await Todo.find({ _id: id });
+
     res.json({
       message: "Todo berhasil didapat",
-      data: todo
-    })
+      data: todo,
+    });
   },
 
   updateTodoById: async (req, res) => {
-    const {id} = req.params
-    let newTodo = req.body
+    const { id } = req.params;
+    let newTodo = req.body;
 
-    const todo = await Todo.findOneAndUpdate({_id: id}, newTodo)
+    const todo = await Todo.findOneAndUpdate({ _id: id }, newTodo);
 
     res.json({
-      message: "Todo berhasil diperbarui"
-    })
-
+      message: "Todo berhasil diperbarui",
+      data: todo,
+    });
   },
 
   deleteTodoById: async (req, res) => {
-    const {id} = req.params
+    const { id } = req.params;
 
-    await Todo.deleteOne({_id: id})
+    await Todo.deleteOne({ _id: id });
 
-    res. json({
-      message: "Todo berhasil dihapus"
-    })
+    res.json({
+      message: "Todo berhasil dihapus",
+    });
   },
 
   deleteAllTodoUser: async (req, res) => {
-    const {username} = req.params
+    const { username } = req.params;
 
-    const id = await User.findOne({username: username})
-    
-    await Todo.deleteMany({userId: id})
+    const id = await User.findOne({ username: username });
+
+    await Todo.deleteMany({ userId: id });
 
     res.json({
-      message: "Semua todo user berhasil dihapus"
-    })
+      message: "Semua todo user berhasil dihapus",
+    });
   },
 
   testRoute: (req, res) => {
--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -2,27 +2,32 @@ const User = require("../models/user");
 
 module.exports = {
   getAllUser: async (req, res) => {
-    const users = await User.find()
+    try {
+      const users = await User.find();
+      if (users.length == 0) throw new Error("Data User Tidak Ditemukan");
 
-    res.json({
+      res.json({
         message: "Data Semua User Telah Didapatkan",
-        data: users
-    })
+        data: users,
+      });
+    } catch (error) {
+      res.json(error.message);
+    }
   },
 
   getUserByUsername: async (req, res) => {
-    const {username} = req.params
+    const { username } = req.params;
 
     try {
-      const user = await User.findOne({username: username})
-      if (!user) throw new Error("User tidak ditemukan")
+      const user = await User.findOne({ username: username });
+      if (!user) throw new Error("User tidak ditemukan");
 
       res.json({
         message: "User berhasil didapat",
-        data: user
-      })
-    } catch (error){
-      res.json(error.message)
+        data: user,
+      });
+    } catch (error) {
+      res.json(error.message);
     }
   },
 
